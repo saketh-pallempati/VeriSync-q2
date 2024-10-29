1359a4909a14063d5298c0fd92dc26119116548d
saketh-pallempati <saketh.pallempati@gmail.com>
2024-10-29 17:47:18 +0530

    Add favicon to HTML head in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 const inter = Inter({ subsets: ["latin"] });
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "ZK Authentication",
@@ -17,9 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="shortcut icon" href="./favicon.ico" />
+      </Head>
       <body className={inter.className}>
         <ThemeProvider attribute="class">
-          <Navbar/>
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
